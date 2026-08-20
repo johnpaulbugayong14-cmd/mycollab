20bdefdd4c661a4ed120057bfbf818e8ade1abd3
John Paul Bugayong <johnpaulbugayong14@gmail.com>
2026-08-20 23:21:27 +0800

    Add files via upload

--- updates/releases/1.0.0/update-config.js
+++ updates/releases/1.0.0/update-config.js
@@ -2,7 +2,7 @@
 export const UPDATE_CONFIG = {
   UPDATE_ENABLED: true,
   DEVELOPMENT_MODE: false,
-  UPDATE_SERVER: 'https://YOUR_USERNAME.github.io/YOUR_REPOSITORY/updates/',
+  UPDATE_SERVER: 'https://github.com/johnpaulbugayong14-cmd/mycollab/tree/main/updates',
   UPDATE_CHECK_INTERVAL_MS: 6 * 60 * 60 * 1000,
   BUNDLED_VERSION: '1.0.0',
   UPDATE_TIMEOUT_MS: 15000,
