7fe7f9e4bf9416e109bf96e8ae8128c83555f8d5
John Paul Bugayong <johnpaulbugayong14@gmail.com>
2026-09-03 23:53:41 +0800

    Add files via upload

---
+++ updates/releases/2.0.0/update-config.js
@@ -0,0 +1,10 @@
+// Web-layer update settings. Change UPDATE_SERVER for your GitHub Pages site.
+export const UPDATE_CONFIG = {
+  UPDATE_ENABLED: true,
+  DEVELOPMENT_MODE: false,
+  UPDATE_SERVER: 'https://johnpaulbugayong14-cmd.github.io/mycollab/updates/',
+  UPDATE_CHECK_INTERVAL_MS: 6 * 60 * 60 * 1000,
+  BUNDLED_VERSION: '1.1.59',
+  UPDATE_TIMEOUT_MS: 15000,
+  KEEP_RELEASES: 2
+};
